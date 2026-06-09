6c5a77542d78992c29f7ec95dbf55166e5338261
andrew dremin <sankoa230@gmail.com>
2026-06-09 14:24:55 +0300

    feat: tokens service

---
+++ packages/types/src/api/response.interface.ts
@@ -0,0 +1,3 @@
+export interface IStandartResponse {
+  message: string;
+}
---
+++ packages/types/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./tokens/tokens.interface.js";
+export * from "./tokens/tokens.schema.js";
+export * from "./api/response.interface.js";
