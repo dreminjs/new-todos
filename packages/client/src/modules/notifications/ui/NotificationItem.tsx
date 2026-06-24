233d9b89d87ee5a538e5737e61758bc0f1a9b2da
andrew dremin <sankoa230@gmail.com>
2026-06-24 11:01:51 +0300

    feat: notifcation system

--- packages/client/src/modules/notifications/ui/NotificationItem.tsx
+++ packages/client/src/modules/notifications/ui/NotificationItem.tsx
@@ -1,11 +1,13 @@
-import type { FC } from "react";
+import { useEffect, type FC } from "react";
 import type {
   INotification,
   TNotificationType,
 } from "../model/notification.interface";
 import styles from "./Notifiction.module.css";
 
-type TProps = INotification;
+type TProps = Omit<INotification, "id"> & {
+  onClear: () => void;
+};
 
 const notificationClasses: Record<TNotificationType, string> = {
   success: styles.notificationItemSuccess,
@@ -14,7 +16,14 @@ const notificationClasses: Record<TNotificationType, string> = {
   warning: styles.notificationItemWarning,
 };
 
-export const NotificationItem: FC<TProps> = ({ id, message, type }) => {
+export const NotificationItem: FC<TProps> = ({ message, type, onClear }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClear();
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [onClear]);
+
   return (
     <li className={notificationClasses[type]}>
       <span>{message}</span>
--- packages/client/src/modules/notifications/ui/NotificationList.tsx
+++ packages/client/src/modules/notifications/ui/NotificationList.tsx
@@ -3,11 +3,18 @@ import { NotificationItem } from "./NotificationItem";
 import styles from "./Notifiction.module.css";
 export const NotificationList = () => {
   const notifications = useNotificationStore((state) => state.notifications);
-
+  const removeNotification = useNotificationStore(
+    (state) => state.removeNotification,
+  );
   return (
     <ul className={styles.notificationList}>
       {notifications.map((notification) => (
-        <NotificationItem key={notification.id} {...notification} />
+        <NotificationItem
+          key={notification.id}
+          message={notification.message}
+          type={notification.type}
+          onClear={() => removeNotification(notification.id)}
+        />
       ))}
     </ul>
   );
